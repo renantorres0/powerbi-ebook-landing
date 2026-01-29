a661521f00587c60f09af8b14c7bc208bb394c8e
Renan Torres <nantorres0@gmail.com>
2026-01-29 01:37:54 -0300

    Ajuste: Adicionando rollupOptions novamente

--- vite.config.ts
+++ vite.config.ts
@@ -164,10 +164,10 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(import.meta.dirname, "client", "index.html"),
+      input: path.resolve(import.meta.dirname, "client/index.html"),
     },
   },
   server: {
